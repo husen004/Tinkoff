915c7019897ac9a69b522290cdd0ccf4588fcf8a
Ikary <husenikary@mail.ru>
2024-08-31 20:47:44 +0500

    Zero COmponent Done

--- src/pages/Zero/Zero.jsx
+++ src/pages/Zero/Zero.jsx
@@ -1,5 +1,10 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { Icon } from 'react-icons-kit'
 import { zerocard } from '../../constants'
+import { FAQzero }  from '../../constants'
+import {arrow_up} from 'react-icons-kit/ikons/arrow_up'
+import {arrow_down} from 'react-icons-kit/ikons/arrow_down'
+import zeroimg3 from '../../assets/images/zeroimg3.webp'
 import DarkBlueButton from '../../components/DarkBlueButton'
 import Button from '../../components/Button'
 import HeadText from '../../components/HeadText'
@@ -9,6 +14,13 @@ import zeroimg2 from '../../assets/images/zeroimg2.webp'
 
 
 const Zero = () => {
+
+  const [open, setOpen] = useState(false)
+
+  function handleOpen()  {
+    setOpen(!open);
+  }
+
   return (
     <div className='w-full'>
       
@@ -48,7 +60,7 @@ const Zero = () => {
           <div className='flex flex-col  max-w-[550px] mx-8'>
             <h4 className='font-bold text-[23px] mt-8'>iOS для начинающих</h4>
             <ul className='mt-2 max-w-[1000px]'>
-              <li className='my-2 font-sans max-w-[]'>- Бесплатное онлайн-обучение iOS-разработке с видеоуроками, практическими заданиями и тестами</li>
+              <li className='my-2 font-sans'>- Бесплатное онлайн-обучение iOS-разработке с видеоуроками, практическими заданиями и тестами</li>
               <li className='my-2 font-sans'>- Обучение в удобном темпе</li>
               <li className='my-2 font-sans'>- Возможность сделать собственное мобильное приложение</li>
               <li className='my-2 font-sans'>- Преподаватели — ведущие iOS-разработчики Т‑Банка</li>
@@ -82,8 +94,46 @@ const Zero = () => {
 
       <HeadText title="Часто задаваемые вопросы" />
 
-      <div className=''>
-        
+      <div className='flex justify-center my-10 mx-2'>
+        <div className='w-[1100px] flex flex-col justify-evenly bg-white rounded-3xl my-6 px-12'>
+          {FAQzero.map((f, index) => (
+            <div className='w-full  cursor-pointer my-6 mx-6 ' onClick={handleOpen} key={index}>
+              <div className='flex flex-row justify-between transition ease-in hover:opacity-70'>
+              <h4 className='font-bold md:text-[23px] text-[14px] transition ease-in hover:opacity-60 '>{f.title}</h4>
+              <div className=' mr-6 cursor-pointer pr-8'>
+                {open ? <Icon icon={arrow_up} size={28} /> : <Icon icon={arrow_down} size={28} />}
+              </div>
+              </div>
+              <div className={`flex items-center ${open ? "block" : "hidden"}`}>
+                <p className='font-sans mt-4 max-w-[950px]'>{f.para}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      
+      <HeadText title="ИТ-курсы для начинающих специалистов" />
+      <div className='flex justify-center my-4'>
+        <p className='font-sans'>Если не нашли здесь подходящий для вашего уровня курс, посмотрите, что еще у нас есть</p>
+      </div>
+
+      <div className='flex justify-center my-10'>
+      <div className='flex flex-col md:flex-row h-full bg-white rounded-3xl '>
+
+        <div className='flex flex-col ml-8 mt-6 '>
+           <h4 className='font-bold text-[23px]'>Т‑Банк Финтех</h4>
+           <p className='font-sans max-w-[500px] my-4'>Бесплатные курсы от топ-менеджеров, техлидов и ведущих разработчиков. Обучаем технологиям, которые используем в компании</p>
+          <div className='flex items-end h-full mb-10'>
+            <button className=' transition ease-in hover:text-blue-700 text-blue-500'>Узнать больше</button>
+          </div>
+        </div>
+
+        <div className='flex h-full items-end my-6 mx-6'>
+          <img src={zeroimg3} className='object-cover w-[528px] h-[230px]' />
+        </div>
+
+      </div>
       </div>
 
     </div>
