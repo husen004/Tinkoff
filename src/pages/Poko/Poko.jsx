050dc8f5b67769613694466a93adb2365adbfd57
Ikary <husenikary@mail.ru>
2024-09-08 13:22:14 +0500

    Add

--- src/pages/Poko/Poko.jsx
+++ src/pages/Poko/Poko.jsx
@@ -1,4 +1,11 @@
 import React from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+import 'swiper/css';
 import { Icon } from 'react-icons-kit'
 import {arrow_up} from 'react-icons-kit/ikons/arrow_up'
 import { jury } from '../../constants'
@@ -148,18 +155,40 @@ const Poko = () => {
 
       <HeadText className="py-6" title="Экспертное жюри" />
 
-      <div>
+      <div className=''>
+      <Swiper
+      modules={[Navigation, Pagination, Scrollbar, A11y]}
+      spaceBetween={20}
+      slidesPerView={2}
+      navigation
+      pagination={{ clickable: true }}
+      a11y={{
+        enabled: true,
+        navigateWithArrowKeys: true,
+      }}
+      >
+      <div className='flex justify-center items-center flex-col gap-10 '>
         {jury.map((j, i) => (
-          <div key={i}>
-            <div>
-              <h4>{j.name}</h4>
-              <p className=''>{j.role}</p>
+      <SwiperSlide>
+          <div className='flex flex-col justify-center rounded-3xl bg-white shadow-xl p-10 h-[400px]' key={i}>
+            <div className='flex flex-row justify-between'>
+              <div className='flex flex-col'>
+                <h4 className='font-bold text-[20px]'>{j.name}</h4>
+                <p className='font-sans text-[14px]'>{j.role}</p>
+              </div>
+              <div className='flex justify-end'>
+               <img src={j.img} className='w-[64px] h-[64px] border-none rounded-3xl border-white' />
+              </div>
             </div>
-            <img src={j.img} className='w-[64px] h-[64px]' />
-            <p className='max-w-[550px]'>{j.para}</p>
+            <p className='max-w-[500px] my-10 '>{j.para}</p>
           </div>
+      </SwiperSlide>
         ))}
       </div>
+      </Swiper>
+      </div>
+
+    <HeadText title="Как стать участником" />
 
 
     </div>
