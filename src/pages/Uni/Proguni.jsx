c63d3cb3d27d9721ab8111e499e81ec86ed7a3c0
Ikary <husenikary@mail.ru>
2024-09-12 16:47:19 +0500

    section done

--- src/pages/Uni/Proguni.jsx
+++ src/pages/Uni/Proguni.jsx
@@ -12,25 +12,25 @@ const Proguni = () => {
           <p className='font-sans my-4 max-w-[300px]'>Базовая кафедра на факультете компьютерных наук: специализция, проекты и спецкурсы</p>
           <button className='absolute bottom-0 mb-8 bg-white rounded-xl shadow-lg w-[180px] h-[50px] transition ease hover:text-gray-500'>Узнать подробнее</button>
           <div className='mt-6'> 
-           <img src={progp1} className='w-[350px] h-[200px]'/>
+           <img src={progp1} className='object-cover w-[360px] h-[240px]'/>
           </div>
         </div>
 
         <div className='relative flex flex-col justify-center items-center text-center transition ease-in hover:scale-105 duration-500 cursor-pointer bg-darkgray rounded-3xl shadow-lg'>
           <h4 className='font-bold text-[23px] mt-8'>Проекты в МФТИ</h4>
-          <p className='font-sans my-4 max-w-[440px]'>Бакалавриат и магистратура в рамках базовой кафедры на ФПМИ МФТИ</p>
+          <p className='font-sans my-4 max-w-[300px]'>Бакалавриат и магистратура в рамках базовой кафедры на ФПМИ МФТИ</p>
           <button className='absolute bottom-0 mb-8 bg-white rounded-xl shadow-lg w-[180px] h-[50px] transition ease hover:text-gray-500'>Узнать подробнее</button>
           <div className='mt-6'> 
-           <img src={progp2} className='h-[240px] w-[528px]'/>
+           <img src={progp2} className='object-cover w-[360px] h-[240px]'/>
           </div>
-        </div>
+        </div> 
 
         <div className='relative flex flex-col justify-center items-center text-center transition ease-in hover:scale-105 duration-500 cursor-pointer bg-darkgray rounded-3xl shadow-lg'>
           <h4 className='font-bold text-[23px] mt-8'>Проекты в МГУ</h4>
-          <p className='font-sans my-4 max-w-[440px]'>Спецкурсы и семинары по программированию, анализу данных и математике</p>
+          <p className='font-sans my-4 max-w-[300px]'>Спецкурсы и семинары по программированию, анализу данных и математике</p>
           <button className='absolute bottom-0 mb-8 bg-white rounded-xl shadow-lg w-[180px] h-[50px] transition ease hover:text-gray-500'>Узнать подробнее</button>
           <div className='mt-6'> 
-           <img src={progp3} className='h-[240px] w-[528px]'/>
+           <img src={progp3} className='object-cover w-[360px] h-[240px]'/>
           </div>
         </div>
 
