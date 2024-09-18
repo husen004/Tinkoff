06dcc8accef05185085a905b9061fcdde037a271
Ikary <husenikary@mail.ru>
2024-09-18 11:54:55 +0500

    Head Text

--- src/pages/Intern/Intern.jsx
+++ src/pages/Intern/Intern.jsx
@@ -23,7 +23,7 @@ const Intern = () => {
         <img src={headintern} alt='intern' className='object-cover w-[80%]'/>
       </div> 
 
-      
+      <HeadText title="Что мы предлагаем" />
 
     </div>
   )
