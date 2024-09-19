5edd1b30a9b62a607fe555840f84c9bc1bfcd58c
Ikary <husenikary@mail.ru>
2024-09-19 23:55:23 +0500

    Changes

--- src/pages/Intern/Develope.jsx
+++ src/pages/Intern/Develope.jsx
@@ -5,14 +5,14 @@ import { develope } from '../../constants/Schoolar'
 const Develope = () => {
   return (
     <div className='tab-animation'>
-      <div className="flex justify-center items-center flex-col">
+      <div className="flex justify-center items-center flex-col gap-8">
         {develope.map((d, index) => (
-          <div className='flex flex-wrap justify-center flex-col' key={index}>
+          <div className='flex justify-center flex-col bg-white rounded-3xl shadow-xl p-6' key={index}>
             
-            <h5 className={`font-sans text-[12px] text-center rounded-2xl w-[100px] ${d.stage === "Набор закрыт" ? "bg-gray-400" : "bg-yellow-400"}`}>{d.stage}</h5>
-            <h2 className='font-semibold text-[22px] max-w-[180px]'>{d.country}</h2>
-            <h2 className='font-semibold text-[22px] max-w-[180px]'>{d.title}</h2>
-            <p className='font-serif'>{d.para}</p>
+            <h5 className={`font-sansserif text-[12px] text-center rounded-2xl w-[100px] ${d.stage === "Набор закрыт" ? "bg-gray-400" : "bg-yellow-400"}`}>{d.stage}</h5>
+            <h2 className='font-semibold text-[22px] max-w-[180px] text-gray-500'>{d.country}</h2>
+            <h2 className='font-semibold text-[22px]'>{d.developer}</h2>
+            <p className='font-serif max-w-[300px]'>{d.para}</p>
             
           </div>  
         ))}
