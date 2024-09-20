3f6fcf7abb59c22a6c9da068371f3eb1a3223d92
Ikary <husenikary@mail.ru>
2024-09-20 11:03:18 +0500

    Uri component Done

--- src/pages/Intern/Uri.jsx
+++ src/pages/Intern/Uri.jsx
@@ -4,17 +4,19 @@ import { uri } from '../../constants/Schoolar'
 
 const Uri = () => {
   return (
-    <div className='tab-animation flex justify-center my-10'>
-      <div className="flex flex-wrap justify-center gap-12 mx-4">
-        {uri.map((d, index) => (
-          <div className='flex justify-center flex-col bg-white rounded-3xl shadow-xl p-6 transition ease-in-out duration-500 md:hover:scale-105 cursor-pointer' key={index}>
+    <div className='tab-animation flex justify-center my-10 mx-4'>
+      <div className='w-[1000px]'>
+        {uri.map((q, index) => (
+          <div key={index}>
+            <div className='flex justify-center flex-col bg-white rounded-3xl shadow-xl p-6 transition ease-in-out duration-500 md:hover:scale-105 cursor-pointer' key={index}>
             
-            <h5 className={`font-sansserif text-[12px] text-center rounded-2xl w-[100px] py-1 ${d.stage === "Набор закрыт" ? "bg-gray-400" : "bg-yellow-400"}`}>{d.stage}</h5>
-            <h2 className='font-semibold text-[22px] max-w-[180px] text-gray-500 my-4'>{d.country}</h2>
-            <h2 className='font-semibold text-[22px]'>{d.developer}</h2>
-            <p className='font-serif max-w-[300px] my-4'>{d.para}</p>
+            <h5 className={`font-sansserif text-[12px] text-center rounded-2xl w-[100px] py-1 ${q.stage === "Набор закрыт" ? "bg-gray-400" : "bg-yellow-400"}`}>{q.stage}</h5>
+            <h2 className='font-semibold text-[22px] text-gray-500 my-4'>{q.country}</h2>
+            <h2 className='font-semibold text-[22px]'>{q.developer}</h2>
+            <p className='font-serif my-4'>{q.para}</p>
             
           </div>  
+          </div>
         ))}
       </div>
     </div>
