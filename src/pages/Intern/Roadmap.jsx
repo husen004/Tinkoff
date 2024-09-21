5511f77a382686903950510de9e802829919ffc8
Ikary <husenikary@mail.ru>
2024-09-21 13:11:41 +0500

    content add

--- src/pages/Intern/Roadmap.jsx
+++ src/pages/Intern/Roadmap.jsx
@@ -24,23 +24,25 @@ const Roadmap = () => {
           <div className='flex flex-col md:flex-row items-center gap-10 text-center'>
             <span className='text-[35px] flex justify-center items-center bg-yellow-300 rounded-xl w-[60px] h-[60px]'>3</span>
             <div className='bg-white rounded-3xl shadow-xl flex flex-col p-6 '>
-            <h4 className='font-semibold text-[20px]'></h4>
-            <p className='fons-sans text-[16] mt-4'></p>
+            <h4 className='font-semibold text-[20px]'>Заполните анкету об опыте и мотивации</h4>
+            <p className='fons-sans text-[16] mt-4 max-w-[500px]'>Доступ к анкете откроется вместе с экзаменами. Каждую анкету изучают кураторы, поэтому мотивация важна так же, как результаты экзамена. </p>
             </div>
           </div>
 
           <div className='flex flex-col md:flex-row items-center gap-10 text-center'>
             <span className='text-[35px] flex justify-center items-center bg-yellow-300 rounded-xl w-[60px] h-[60px]'>4</span>
             <div className='bg-white rounded-3xl shadow-xl flex flex-col p-6'>
-            <h4 className='font-semibold text-[20px]'></h4>
-            <p className='fons-sans text-[16] mt-4'></p>
+            <h4 className='font-semibold text-[20px]'>Пройдите онлайн-экзамен</h4><br/>
+            <p className='font-sans'>Перед экзаменом можно попробовать свои силы, решив <span className='cursor-pointer text-blue-400'>тренировочный контест</span></p><br/>
+            <p className='font-sans'>— Материалы для подготовки ищите в сообществе во ВКонтакте</p>  
+            <p className='fons-sans'>— Вопросы можно задать в телеграм-боте @clar_tbot</p>
             </div>
           </div>
           <div className='flex flex-col md:flex-row items-center gap-10 text-center'>
             <span className='text-[35px] flex justify-center items-center bg-yellow-300 rounded-xl w-[60px] h-[60px]'>5</span>
             <div className='bg-white rounded-3xl shadow-xl flex flex-col p-6'>
-            <h4 className='font-semibold text-[20px]'></h4>
-            <p className='fons-sans text-[16] mt-4'></p>
+            <h4 className='font-semibold text-[20px]'>Пройдите интервью с одной или несколькими командами</h4>
+            <p className='fons-sans text-[16] mt-4 max-w-[500px]'>Координатор пригласит всех, кто успешно справился с отборочными испытаниями. После интервью — свяжется с вами и сообщит результаты</p>
             </div>
           </div>
         </div>
