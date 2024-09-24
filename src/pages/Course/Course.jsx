382ad46afe945422b39b88b1510b235bc862689e
Ikary <husenikary@mail.ru>
2024-09-24 10:35:44 +0500

    Hover Element Chnage

--- src/pages/Course/Course.jsx
+++ src/pages/Course/Course.jsx
@@ -19,7 +19,7 @@ const Course = () => {
 
       <div className='flex flex-wrap justify-center items-center gap-10 my-14 mx-4'>
 
-        <div className='relative flex flex-col justify-center items-center text-center transition ease-in-out duration-500 hover:scale-105 cursor-pointer bg-white rounded-3xl shadow-lg'>
+        <div className='relative flex flex-col justify-center items-center text-center transition ease-in-out duration-500 md:hover:scale-105 cursor-pointer bg-white rounded-3xl shadow-lg'>
           <h4 className='font-bold text-[23px] mt-8'>Для начинающих</h4>
           <p className='font-sans my-4 max-w-[440px]'>Образовательные курсы по разработке, аналитике, SRE, QA, маркетингу, дизайну и другим направлениям</p>
           <button className='absolute bottom-0 mb-8 bg-white rounded-xl shadow-lg w-[120px] h-[50px] transition ease hover:text-gray-500'>Подробнее</button>
@@ -28,7 +28,7 @@ const Course = () => {
           </div>
         </div>
 
-        <div className='relative flex flex-col justify-center items-center text-center transition ease-in-out hover:scale-105 duration-500 cursor-pointer bg-white rounded-3xl shadow-lg'>
+        <div className='relative flex flex-col justify-center items-center text-center transition ease-in-out md:hover:scale-105 duration-500 cursor-pointer bg-white rounded-3xl shadow-lg'>
           <h4 className='font-bold text-[23px] mt-8'>Для опытных</h4>
           <p className='font-sans my-4 max-w-[440px]'>Интенсивы для разработчиков, аналитиков, менеджеров и других специалистов </p>
           <button className='absolute bottom-0 mb-8 bg-white rounded-xl shadow-lg w-[120px] h-[50px] transition ease hover:text-gray-500'>Подробнее</button>
