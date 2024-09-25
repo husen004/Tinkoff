3c6a3513e40ed9bdf5ba8b76ac11eb1447a4a1e2
Ikary <husenikary@mail.ru>
2024-09-25 10:11:22 +0500

    Bug Fixed

--- src/pages/Intern/Internstar.jsx
+++ src/pages/Intern/Internstar.jsx
@@ -6,8 +6,8 @@ const Internstar = () => {
   return (
     <div className='flex flex-col justify-center items-center mx-4'>
 
-        <div className='flex flex-col md:flex-row justify-center items-center gap-10 my-4'>
-            <div className='flex flex-col md:flex-row bg-white rounded-3xl shadow-xl'>
+        <div className='flex flex-col md:flex-row justify-center items-center gap-10 my-4 mt-10'>
+            <div className='flex flex-col md:flex-row bg-white rounded-3xl shadow-xl py-[2px]'>
               <div className='flex flex-col pt-6 pl-6'>
               <h1 className='font-semibold text-[22px]'>1. Первый день</h1>
               <p className='font-sans text-[15px] mt-4 max-w-[300px]'>Получите рабочий ноутбук, подпишете документы, познакомитесь с командой и с экосистемой Т‑Банка, договоритесь о графике и формате работы</p>
@@ -24,7 +24,7 @@ const Internstar = () => {
         </div>
 
         <div className='flex flex-col md:flex-row justify-center items-center gap-10 my-4'> 
-            <div className='flex flex-row bg-white rounded-3xl shadow-xl'>
+            <div className='flex flex-row bg-white rounded-3xl shadow-xl py-1'>
               <div className='flex flex-col justify-start items-start pb-6 pl-6 my-[20px]'>
               <h1 className='font-semibold text-[22px]'>3. Основная работа над <br />проектом</h1>
               <p className='font-sans text-[15px] mt-4 max-w-[300px]'>Вместе с командой будете шаг за шагом выполнять бизнес-задачи</p>
