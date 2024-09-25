797abc8fc03523287720574d4a84924b183e921e
Ikary <husenikary@mail.ru>
2024-09-25 10:05:24 +0500

    Change animation

--- src/pages/Intern/Uri.jsx
+++ src/pages/Intern/Uri.jsx
@@ -4,7 +4,7 @@ import { uri } from '../../constants/Schoolar'
 
 const Uri = () => {
   return (
-    <div className='tab-animation flex justify-center my-10 mx-4'>
+    <div className='slide-tabs flex justify-center mb-16 mx-4'>
       <div className='w-[1000px]'>
         {uri.map((q, index) => (
           <div key={index}>
