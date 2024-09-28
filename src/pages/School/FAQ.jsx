168c545db88d61b789198b98999ad164864f1701
Ikary <husenikary@mail.ru>
2024-09-28 12:45:05 +0500

    Changes

--- src/pages/School/FAQ.jsx
+++ src/pages/School/FAQ.jsx
@@ -3,6 +3,7 @@ import { schoolFAQ } from '../../constants'
 import { Icon } from 'react-icons-kit'
 import {arrow_up} from 'react-icons-kit/ikons/arrow_up'
 import {arrow_down} from 'react-icons-kit/ikons/arrow_down'
+import '../../index.css'
 
 const FAQ = () => {
 
@@ -24,7 +25,7 @@ const FAQ = () => {
                 <div className='flex flex-row justify-between transition ease-in hover:opacity-60'>
                 <h4 className='font-semibold md:text-[23px] text-[14px] transition ease-in hover:opacity-60 '>{s.title}</h4>
                 <div className={`pr-8`}>
-                  {open === i ? <Icon icon={arrow_up} size={28} /> : <Icon icon={arrow_down} size={28} />}
+                  {open === i ? <Icon icon={arrow_up} size={28} /> : <Icon icon={arrow_up} size={28} />}
                 </div>
                 </div>
               <div className={`grid overflow-hidden transition-all duration-300 ease-in-out  ${open === i ? "grid-rows-[1fr] opacity-100" : "grid-rows-[0fr] opacity-0"}`}>
